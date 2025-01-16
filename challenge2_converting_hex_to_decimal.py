7d34c92a499338e701be339cdcd4460cd213fff0
Leon Lusbo <leonlusbo11@gmail.com>
2025-01-17 01:53:36 +1100

    modified challenge2_converting_hex_to_decimal.py

---
+++ challenge2_converting_hex_to_decimal.py
@@ -0,0 +1,10 @@
+# Python code​​​​​​‌‌​​​‌‌‌‌‌‌​​​‌​​​‌​​‌‌‌‌ below
+hexNumbers = {
+    '0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+    'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15
+}
+
+# Converts a string hexadecimal number into an integer decimal
+# If hexNum is not a valid hexadecimal number, returns None
+def hexToDec(hexNum):
+    pass
