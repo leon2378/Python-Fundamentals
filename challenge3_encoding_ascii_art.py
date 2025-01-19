f6d21d951eace0d04bbeb1cc5d1028742e2a1d5d
Leon Lusbo <leonlusbo11@gmail.com>
2025-01-19 19:53:02 +1100

    modified challenge3_encoding_ascii_art.py

---
+++ challenge3_encoding_ascii_art.py
@@ -0,0 +1,9 @@
+# Python code​​​​​​‌‌​​‌​​​​​​‌‌‌‌​‌​‌​​​​​‌ below
+
+def encodeString(stringVal):
+    # Your code goes here.
+    pass
+
+def decodeString(encodedList):
+    # Your code goes here.
+    pass
