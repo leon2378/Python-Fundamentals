497f17d93c1a4b9251a106eab8292bfebedb07a4
Leon Lusbo <leonlusbo11@gmail.com>
2025-01-24 17:19:37 +1100

    Modified challenge5_sum_of_triangles.py

---
+++ challenge5_sum_of_triangles.py
@@ -0,0 +1,12 @@
+# Python code​​​​​​‌‌​​‌​​​​​‌​‌​​‌‌​​​‌‌​‌‌ below
+def triangle(num):
+    if num == 1:
+        return num
+    return num + triangle(num - 1)
+
+def square(num):
+    if num == 0:
+        return 0  # 0^2 = 0
+    if num == 1:
+        return 1  # 1^2 = 1
+    return triangle(num) + triangle(num - 1)
