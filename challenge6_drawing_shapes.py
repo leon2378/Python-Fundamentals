c4d8955560a91c7c1baa8828483ddadde34b04bb
Leon Lusbo <leonlusbo11@gmail.com>
2025-01-27 21:59:16 +1100

    Modified challenge6_drawing_shapes.py

--- challenge6_drawing_shapes.py
+++ challenge6_drawing_shapes.py
@@ -30,3 +30,14 @@ class Shape:
     width = 5
     height = 5
     printChar = '#'
+
+    def printRow(self, i):
+        raise NotImplementedError("Will be implemented by children extending this class")
+    
+    def print(self):
+        for i in range(self.height):
+            self.printRow(i)
+
+class Square(Shape):
+    def printRow(self, i):
+        print(self.printChar * self.width)
