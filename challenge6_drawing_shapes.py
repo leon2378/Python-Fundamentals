62fe28e89254f745ea3a61317a587d70361a7056
Leon Lusbo <leonlusbo11@gmail.com>
2025-01-27 21:53:05 +1100

    Modified challenge6_drawing_shapes.py

--- challenge6_drawing_shapes.py
+++ challenge6_drawing_shapes.py
@@ -1,20 +1,24 @@
-# Python code​​​​​​‌‌​​‌​​​‌‌​‌‌​‌‌‌​​​‌​​‌​ below
 class Shape:
-	width = 5
-	height = 5
-	printChar = '#'
+    def __init__(self, width=5, height=5, printChar='#'):
+        self.width = width
+        self.height = height
+        self.printChar = printChar
 
-	def printRow(self, i):
-		raise NotImplementedError("Will be implemented by children extending this class")
+    def printRow(self, i):
+        raise NotImplementedError("Will be implemented by children extending this class")
 
-	def print(self):
-		for i in range(self.height):
-			self.printRow(i)
+    def print(self):
+        for i in range(self.height):
+            self.printRow(i)
 
 
 class Square(Shape):
-	def printRow(self, i):
-		print(self.printChar * self.width)
+    def printRow(self, i):
+        # Print a full row of printChar repeated width times
+        print(self.printChar * self.width)
+
 
 class Triangle(Shape):
-	pass
+    def printRow(self, i):
+        # Print i + 1 printChars to form a right-angled triangle
+        print(self.printChar * (i + 1))
