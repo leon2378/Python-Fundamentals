5f028bcddc6cfcc44f793f6d3d1f33ce818c1935
Leon Lusbo <leonlusbo11@gmail.com>
2025-02-01 23:43:27 +1100

    Modified challenge7_bad_arguments.py

--- challenge7_bad_arguments.py
+++ challenge7_bad_arguments.py
@@ -16,3 +16,17 @@ def handleNonIntArguments(func):
 @handleNonIntArguments
 def my_sum(*args):
     return sum(args)
+
+
+# Second Method
+
+class NonIntArgumentException(Exception):
+    pass
+
+def handleNonIntArguments(func):
+    def wrapper(*args):
+        for item in args:
+            if type(item) is not int:
+                raise NonIntArgumentException()
+        return func(*args)
+    return wrapper
