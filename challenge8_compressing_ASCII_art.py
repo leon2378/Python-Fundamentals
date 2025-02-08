88a968ccfe8248f3b5cb306b36639ec566f5c660
Leon Lusbo <leonlusbo11@gmail.com>
2025-02-08 22:18:47 +1100

    Modified challenge8_compressing_ASCII_art.py

---
+++ challenge8_compressing_ASCII_art.py
@@ -0,0 +1,32 @@
+# Python code​​​​​​‌‌​​‌​‌​​​‌‌‌‌​​‌​‌​‌‌​‌‌ below
+import json 
+
+def encodeString(stringVal):
+    encodedList = []
+    prevChar = None
+    count = 0
+    for char in stringVal:
+        if prevChar != char and prevChar is not None:
+            encodedList.append((prevChar, count))
+            count = 0
+        prevChar = char
+        count = count + 1
+    encodedList.append((prevChar, count))
+    return encodedList
+
+def decodeString(encodedList):
+    decodedStr = ''
+    for item in encodedList:
+        decodedStr = decodedStr + item[0] * item[1]
+    return decodedStr
+
+# The filename that will be passed to this function
+# is 10_04_challenge_art.txt
+def encodeFile(filename, newFilename):
+    # Your code goes here.
+    pass
+
+def decodeFile(filename):
+    # Your code also goes here.
+    pass
+    
