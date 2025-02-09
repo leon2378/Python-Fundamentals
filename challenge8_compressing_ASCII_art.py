2c89be2aed2d78d7b72b19af11a09a894d3e44ac
Leon Lusbo <leonlusbo11@gmail.com>
2025-02-09 22:42:12 +1100

    Modified challenge8_compressing_ASCII_art.py

--- challenge8_compressing_ASCII_art.py
+++ challenge8_compressing_ASCII_art.py
@@ -107,3 +107,29 @@ def decodeFile(filename):
     pairs = [p.split('|') for p in pairs]
     pairs = [(p[0], int(p[1])) for p in pairs]
     return decodeString(pairs)
+
+
+# Fourth Method (Byte solution)
+
+def encodeFile(filename, newFilename):
+    with open(filename) as f:
+        data = encodeString(f.read())
+    output = bytearray()
+    for item in data:
+        # Character byte
+        output.extend(bytes(item[0], 'utf-8'))
+        # integer count byte
+        output.extend(item[1].to_bytes(1, 'utf-8'))
+    with open(newFilename, 'wb') as binary_file:
+        # write bytes to file
+        binary_file.write(output)
+
+def decodeFile(filename):
+    with open(filename, 'rb') as f:
+        data = f.read()
+        # split data into pairs
+        bytePairs = [data[i:i+2] for i in range (0, len(data), 2)]
+        encodedList = []
+        for bytePair in bytePairs:
+            encodedList.append((bytePair[:1].decode('utf-8'), int.from_bytes(bytePair[1:], 'big')))
+        return decodeString(encodedList)
