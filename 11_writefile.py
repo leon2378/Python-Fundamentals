bf1f17e31da8f38f7d3f299f35889be7f372a678
Leon Lusbo <leonlusbo11@gmail.com>
2025-02-09 22:55:55 +1100

    Modified 11_writefile.py

--- 11_writefile.py
+++ 11_writefile.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
-parser.add_argument('--output')
+parser.add_argument('--output', required=True, help='The destination file for the output of this program')
 
 args = parser.parse_args()
 
